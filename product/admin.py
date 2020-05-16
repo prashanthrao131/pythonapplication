68ebedb072a8ccbcb08c3993502af7e96a674835
piyumi dasanayaka <piyumi.m.dasanayaka@gmail.com>
2020-05-16 09:24:43 +0530

    display offers in admin

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
 
-#inherit from  ModelAdmin class in admin
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+# inherit from  ModelAdmin class in admin
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','price','stock')
+    list_display = ('name', 'price', 'stock')
 
 
+# register
+admin.site.register(Offer, OfferAdmin)
 admin.site.register(Product, ProductAdmin)
