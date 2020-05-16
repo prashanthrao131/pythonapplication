8129a9b59998925c5fdbe9b426e34c8e360833ec
piyumi dasanayaka <piyumi.m.dasanayaka@gmail.com>
2020-05-16 09:17:12 +0530

    display product table to admin

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+#inherit from  ModelAdmin class in admin
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
+
+
+admin.site.register(Product, ProductAdmin)
